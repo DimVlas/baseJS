49a1c1ea6a590401afecf2a8320d318fda882431
DimVlas <D.V.Vlasenko@gmail.com>
2026-04-13 14:26:46 +0300

    02. event listener

--- script.js
+++ script.js
@@ -1,34 +1,33 @@
-let div = document.querySelector('.some')
 
-// // // вывод свойств элемента в консоль
-// // console.dir(div) 
+// #1
+// window.onload = function(){
+//     let div = document.querySelector('.some');
+//     div.innerHTML = '1';
+// }
 
-// // заменили содержимое элемента
-// div.innerHTML = 'replaced <div>text</div>'
+// #2
+// function pageLoaded() {
+//     let div = document.querySelector('.some');
+//     div.innerHTML = '1';
+// }
+// window.onload = pageLoaded;
 
-// // переопредилили цвет содержимого элемента
-// div.style.color = 'red'
+// #3
+// window.addEventListener('load', function(){
+//     let div = document.querySelector('.some');
+//     div.innerHTML = '111';
+// })
 
-// // использование метода добавления нового дочернего элемента к текущему элементу
-// let p = div.appendChild(document.createElement('p'))
-// p.innerText = 'добавленный абзац'
+//4
+// function pageLoaded() {
+//     let div = document.querySelector('.some');
+//     div.innerHTML = '111';
+// }
+// window.addEventListener('load', pageLoaded);
 
-// подписываемся на событие нажатия кнопки
-// ДЗ: скрываем/показываем div по нажанию
-let btn = document.querySelector('button')
-btn.onclick = function () {
-    // div.classList.add('active')
-    // div.style.color = 'green'
-    // div.innerHTML = 'an event occurred'
-    
-    // if (div.classList.contains('hidden_div')){
-    //     div.classList.remove('hidden_div')
-    //     div.classList.add('visible_div')
-    // }else{
-        
-    //     div.classList.remove('visible_div')
-    //     div.classList.add('hidden_div')
-    // }
-
-    div.classList.toggle('hidden_div');
-}
+// ДЗ
+// 1. переписать Дз прошлого урока в канонический вариант
+// 2. создать в html тэг input, 
+//    подипсаться на событие ввода данных в input
+//    и выводить их в консоль. 
+//    И еще кнопку очистки input
