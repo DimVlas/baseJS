3eae82a6e218b1261ed3fa4a3dd842863340f2ca
DimVlas <D.V.Vlasenko@gmail.com>
2026-04-15 11:17:15 +0300

    02. homework

--- script.js
+++ script.js
@@ -31,3 +31,45 @@
 //    подипсаться на событие ввода данных в input
 //    и выводить их в консоль. 
 //    И еще кнопку очистки input
+
+function btnClick() {
+    let div = document.querySelector('.some')
+    div.classList.toggle('hidden_div');
+
+    let inp = document.querySelector('input');
+    inp.value = "";
+}
+
+function pageLoaded() {
+    let btn = document.querySelector('button');
+    btn.addEventListener('click', btnClick);
+
+    let inp = document.querySelector('input');
+    console.log(inp);
+    //inp.value = 'aaaaaaaaaaa';
+    inp.oninput = function(ev){
+        console.log(inp.value);
+    }
+
+}
+
+window.addEventListener('load', pageLoaded);
+
+/*
+let btn = document.querySelector('button')
+btn.onclick = function () {
+    // div.classList.add('active')
+    // div.style.color = 'green'
+    // div.innerHTML = 'an event occurred'
+    
+    if (div.classList.contains('hidden_div')){
+        div.classList.remove('hidden_div')
+        div.classList.add('visible_div')
+    }else{
+        
+        div.classList.remove('visible_div')
+        div.classList.add('hidden_div')
+    }
+
+}
+*/
