1078785b89acb13b17e05ef993ee3c7a5933ea11
DimVlas <D.V.Vlasenko@gmail.com>
2026-04-15 12:42:12 +0300

    03. homework

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Базовый javascript.
 
 https://rutube.ru/plst/1142480?r=wd
+
+lesson_04: https://rutube.ru/video/294f8f49b79828d01415cdb598d06951/?playlist=1142480
--- script.js
+++ script.js
@@ -1,76 +1,33 @@
-
-// #1
-// window.onload = function(){
-//     let div = document.querySelector('.some');
-//     div.innerHTML = '1';
-// }
-
-// #2
-// function pageLoaded() {
-//     let div = document.querySelector('.some');
-//     div.innerHTML = '1';
-// }
-// window.onload = pageLoaded;
-
-// #3
-// window.addEventListener('load', function(){
-//     let div = document.querySelector('.some');
-//     div.innerHTML = '111';
-// })
-
-//4
-// function pageLoaded() {
-//     let div = document.querySelector('.some');
-//     div.innerHTML = '111';
-// }
-// window.addEventListener('load', pageLoaded);
-
-// ДЗ
-// 1. переписать Дз прошлого урока в канонический вариант
-// 2. создать в html тэг input, 
-//    подипсаться на событие ввода данных в input
-//    и выводить их в консоль. 
-//    И еще кнопку очистки input
-
-function btnClick() {
-    let div = document.querySelector('.some')
-    div.classList.toggle('hidden_div');
-
-    let inp = document.querySelector('.inp');
-    inp.value = "";
-}
-
 function pageLoaded() {
-    let btn = document.querySelector('button');
-    btn.addEventListener('click', btnClick);
+    let btnSet = document.querySelector(".btnSet");
+    let btnAdd = document.querySelector(".btnAdd");
+    let colors = [];
 
-    let inp = document.querySelector('.inp');
-    console.log(inp);
-    inp.addEventListener('input', function (e) {
-        console.log(inp.value);
-        console.log(this.value);
-        console.log(e.target.value);
+    btnSet.addEventListener('click', function () {
+        let toolTip = document.querySelector('.toolTip');
+        
+        if (colors.length < 1) {
+            console.log('color not found');
+
+            toolTip.style.visibility = 'visible';
+            return;
+        }
+
+        toolTip.style.visibility = 'hidden';
+        let color = colors[rand(colors.length)];
+        console.log(color);
+        btnSet.style.color = color;
     });
 
+    btnAdd.addEventListener('click', function () {
+        colors.push(`rgb(${rand(256)},${rand(256)},${rand(256)})`);
+        console.log(colors);
+    });
 }
 
 window.addEventListener('load', pageLoaded);
 
-/*
-let btn = document.querySelector('button')
-btn.onclick = function () {
-    // div.classList.add('active')
-    // div.style.color = 'green'
-    // div.innerHTML = 'an event occurred'
-    
-    if (div.classList.contains('hidden_div')){
-        div.classList.remove('hidden_div')
-        div.classList.add('visible_div')
-    }else{
-        
-        div.classList.remove('visible_div')
-        div.classList.add('hidden_div')
-    }
-
+/** случайное число */
+function rand(max) {
+    return Math.floor(Math.random() * max);
 }
-*/
