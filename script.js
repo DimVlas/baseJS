c1b1b36224326905ee1834b1ada6c82c7c25f7af
DimVlas <D.V.Vlasenko@gmail.com>
2026-04-16 16:06:25 +0300

    04. homework

--- script.js
+++ script.js
@@ -1,58 +1,32 @@
-// function pageLoaded() {
-//     let btnSet = document.querySelector(".btnSet");
-//     let btnAdd = document.querySelector(".btnAdd");
-//     let colors = [];
-
-//     btnSet.addEventListener('click', function () {
-//         let toolTip = document.querySelector('.toolTip');
-
-//         if (colors.length < 1) {
-//             console.log('color not found');
-
-//             toolTip.style.visibility = 'visible';
-//             return;
-//         }
-
-//         toolTip.style.visibility = 'hidden';
-//         let color = colors[rand(colors.length)];
-//         console.log(color);
-//         btnSet.style.color = color;
-//     });
-
-//     btnAdd.addEventListener('click', function () {
-//         colors.push(`rgb(${rand(256)},${rand(256)},${rand(256)})`);
-//         console.log(colors);
-//     });
-// }
-
 // решение преподавателя
 function pageLoaded() {
+    let alertSome = document.querySelector(".alert-some");
+    let alertOther = document.querySelector(".alert-other");
+    let btnAdd = document.querySelector('.btnAdd');
 
-    let btnSet = document.querySelector(".btnSet");
-    let btnAdd = document.querySelector(".btnAdd");
-    let toolTip = document.querySelector('.alert');
-    let colors = [];
+    alertSome.addEventListener('click', setRandomColor);
 
-    btnSet.addEventListener('click', function () {
+    alertOther.addEventListener('mousewheel', setRandomColor);
 
-        if (colors.length < 1) {
-            toolTip.classList.remove('hide');
-            return;
-        }
-
-        let color = colors[rand(colors.length)];
-        btnSet.style.color = color;
-    });
-
-    btnAdd.addEventListener('click', function () {        
-        toolTip.classList.add('hide');
-        colors.push(`rgb(${rand(256)},${rand(256)},${rand(256)})`);
-    });
+    btnAdd.addEventListener('click', addRandomColor);
 }
+
+let colors = ['red', 'green', 'blue'];
 
 window.addEventListener('load', pageLoaded);
 
 /** случайное число */
 function rand(max) {
     return Math.floor(Math.random() * max);
+}
+
+function addRandomColor() {
+    colors.push(`rgb(${rand(256)},${rand(256)},${rand(256)})`);
+}
+
+function setRandomColor() {
+    let ind = rand(colors.length);
+    let color = colors[ind];
+
+    this.style.color = color;
 }
