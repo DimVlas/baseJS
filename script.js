24cdcbcf90b815915c51313592962809d6f1a4b2
DimVlas <D.V.Vlasenko@gmail.com>
2026-04-16 13:08:53 +0300

    03. homework. solution from the teacher

--- script.js
+++ script.js
@@ -1,27 +1,52 @@
+// function pageLoaded() {
+//     let btnSet = document.querySelector(".btnSet");
+//     let btnAdd = document.querySelector(".btnAdd");
+//     let colors = [];
+
+//     btnSet.addEventListener('click', function () {
+//         let toolTip = document.querySelector('.toolTip');
+
+//         if (colors.length < 1) {
+//             console.log('color not found');
+
+//             toolTip.style.visibility = 'visible';
+//             return;
+//         }
+
+//         toolTip.style.visibility = 'hidden';
+//         let color = colors[rand(colors.length)];
+//         console.log(color);
+//         btnSet.style.color = color;
+//     });
+
+//     btnAdd.addEventListener('click', function () {
+//         colors.push(`rgb(${rand(256)},${rand(256)},${rand(256)})`);
+//         console.log(colors);
+//     });
+// }
+
+// решение преподавателя
 function pageLoaded() {
+
     let btnSet = document.querySelector(".btnSet");
     let btnAdd = document.querySelector(".btnAdd");
+    let toolTip = document.querySelector('.alert');
     let colors = [];
 
     btnSet.addEventListener('click', function () {
-        let toolTip = document.querySelector('.toolTip');
-        
-        if (colors.length < 1) {
-            console.log('color not found');
 
-            toolTip.style.visibility = 'visible';
+        if (colors.length < 1) {
+            toolTip.classList.remove('hide');
             return;
         }
 
-        toolTip.style.visibility = 'hidden';
         let color = colors[rand(colors.length)];
-        console.log(color);
         btnSet.style.color = color;
     });
 
-    btnAdd.addEventListener('click', function () {
+    btnAdd.addEventListener('click', function () {        
+        toolTip.classList.add('hide');
         colors.push(`rgb(${rand(256)},${rand(256)},${rand(256)})`);
-        console.log(colors);
     });
 }
 
