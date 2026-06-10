7bb1f4d578ccd56bb9691d8f942ce38226c05bf1
DimVlas <D.V.Vlasenko@gmail.com>
2026-06-10 12:41:17 +0300

    05. homework

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
-
-window.addEventListener('load', pageLoaded);
+window.addEventListener("load", pageLoaded);
 
 function pageLoaded() {
-    // let list = document.querySelector('.list'); // элемент <ul>
-    let list = document.querySelectorAll('.list li'); // коллекция элементов <li>
+  // let list = document.querySelector('.list'); // элемент <ul>
+  let list = document.querySelectorAll(".list li"); // коллекция элементов <li>
 
-    // как работать с такой коллекцией
-    /*
+  // как работать с такой коллекцией
+  /*
     // Обычный цикл
     for (let i = 0; i < list.length; i++) {
         console.dir(list[i]);
     }
     */
 
-    /*
+  /*
     // // цикл foreach требует call back, которые ще не проходили, поэтому потом
     list.foreach(...);
     */
 
-    /*
-    // вспомним, что объекты в js, это ассоциативный массив (маппа), 
+  /*
+    // вспомним, что объекты в js, это ассоциативный массив (маппа),
     // где ключи, это наименование свойств.
     // цикл for in вернет, кроме индексов элементов, еще доп ключи-свойства
     for (let key in list) {
@@ -27,17 +26,19 @@ function pageLoaded() {
     }
     */
 
-    /*
+  /*
     // цикл for of перебирает итерируемые сущности
     // по сути, как раз элементы нашей коллекции
     for (let el of list) {
         el.addEventListener('mouseenter', setElemAsActive);
     }*/
 
-    let asks = document.querySelectorAll('.faq .ask');
-    for (let ask of asks) {
-        ask.addEventListener('click', toggleAnswer);
-    }
+  let asks = document.querySelectorAll(".faq .ask");
+  for (let ask of asks) {
+    // ask.addEventListener("click", toggleAnswer);
+
+    ask.addEventListener("click", clickAsk);
+  }
 }
 
 /*
@@ -48,27 +49,56 @@ function setElemAsActive() {
 */
 
 function toggleAnswer() {
-    console.log(this); // элемент, в контексте которого событие
-    console.log(this.parentNode); // родительский элемент
+  console.log(this); // элемент, в контексте которого событие
+  console.log(this.parentNode); // родительский элемент
 
-    // попробуем найти нужный элемент answer
-    console.log(this.parentNode.children[1]); // первый элемент в родительском
-    console.log(this.nextSibling); // в данном случае вернет переход строки - элемент между двума элементами div
-    console.log(this.nextElementSibling);  // а это уже вернет следующий элемент дерева DOM, игнорируя текстовые узлы
-    // но все эти попытки ломаются, как только меняется html-разметка
+  // попробуем найти нужный элемент answer
+  console.log(this.parentNode.children[1]); // первый элемент в родительском
+  console.log(this.nextSibling); // в данном случае вернет переход строки - элемент между двума элементами div
+  console.log(this.nextElementSibling); // а это уже вернет следующий элемент дерева DOM, игнорируя текстовые узлы
+  // но все эти попытки ломаются, как только меняется html-разметка
 
-    // самый надежный
-    console.log(this.parentNode.querySelector('.answer'));
-    // но тут тож есть проблема: 
-    //         если, вдруг, div ask окажется обернутым другим элеентом, все сломается
-    // Для рещения этой проблемы есть функция, котораю идет по дереву DOM вверх, 
-    // и ищет элемент нужного класса
-    console.log(this.closest('.item')); // найдем родительский div item
-    // и затем в нем найдем по классу answer
-    console.log(this.closest('.item').querySelector('.answer')); // BINGO !!!
+  // самый надежный
+  console.log(this.parentNode.querySelector(".answer"));
+  // но тут тож есть проблема:
+  //         если, вдруг, div ask окажется обернутым другим элеентом, все сломается
+  // Для рещения этой проблемы есть функция, котораю идет по дереву DOM вверх,
+  // и ищет элемент нужного класса
+  console.log(this.closest(".item")); // найдем родительский div item
+  // и затем в нем найдем по классу answer
+  console.log(this.closest(".item").querySelector(".answer")); // BINGO !!!
 
-    this.closest('.item').querySelector('.answer').classList.toggle('hide');
+  // из всего вышесказанного
+  // в div 2 элемента разных классов
+  // по клику на одно элементе, надо найти другой элемент
+  // наилучший вариант использовать closest('.item')
+  // closest('.item') - ищет в дереве DOM вверх, пока не найдет элемент нужного класса
+  // и затем в нем найдем по классу нужный нам
+  // this.closest(".item").querySelector(".answer").classList.toggle("hide");
 }
 
 // ДЗ: Сделать "аккордеон" - когда открывается 1 элемент, закрываются остальные,
 //  если кликают по открытому, то ничего не происходит
+function clickAsk() {
+  let ans = this.closest(".item").querySelector(".answer");
+  if (ans == null) return;
+
+  console.log(ans);
+  // ans.classList.toggle("hide");
+  //
+  console.log(ans.classList.contains("hide"));
+
+  if (!ans.classList.contains("hide")) {
+    return;
+  }
+
+  let otherAns = this.closest(".item")
+    .closest(".faq")
+    .querySelectorAll(".answer");
+  console.log(otherAns);
+  for (let a of otherAns) {
+    a.classList.add("hide");
+  }
+
+  ans.classList.remove("hide");
+}
